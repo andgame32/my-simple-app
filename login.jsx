acd33feed565bce360894104f237bb317a10e213
andgame32 <andgame32pelmen@gmail.com>
2026-05-06 09:33:04 +0800

    Update login.jsx

--- login.jsx
+++ login.jsx
@@ -54,3 +54,17 @@ return(
 )
 }
 export default LoginForm;
+
+
+
+
+
+
+INSERT INTO users (name, email, password, created_at, updated_at)
+VALUES (
+    'Test User',
+    'test@example.com',
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og9u0FhQ5dYyZ8eW',
+    NOW(),
+    NOW()
+);
